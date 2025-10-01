9caaf6ce3b17feb2628dd1ebd1122886db5dbea0
Chloe Hwang <chloesuyeon.h@gmail.com>
2025-10-01 16:01:38 +1000

    Chloe Updated 1st Oct 4pm

--- src/Community.tsx
+++ src/Community.tsx
@@ -15,15 +15,15 @@ const Community:FunctionComponent = () => {
   	return (
     		<div className={styles.community}>
       			<div className={styles.post}>
-        				<span>{`I feel like I haven’t slept in days 😅 My 6-week-old cries every time I put her down in the crib. Is this just .... `}</span>
+        				<span>{`I feel like I haven’t slept in days 😅 My 6-week-old cries every time I put her down in the crib. Is this.... `}</span>
         				<span className={styles.seeMore}>See more</span>
       			</div>
       			<div className={styles.communityPost}>
-        				<span>{`I’m 20 weeks pregnant and missing my morning coffee so much ☕😩 Some friends say one cup a day is ok.... `}</span>
+        				<span>{`I’m 20 weeks pregnant and missing my morning coffee so much ☕😩 Some friends say one cup a day is.... `}</span>
         				<b className={styles.communitySeeMore}>See more</b>
       			</div>
       			<div className={styles.post2}>
-        				<span>{`My 4-year-old loves cartoons, but I’m worried I let him watch too much. What’s your daily screen time limit for.... `}</span>
+        				<span>{`My 4-year-old loves cartoons, but I’m worried I let him watch too much. What’s your daily screen.... `}</span>
         				<b className={styles.communitySeeMore}>See more</b>
       			</div>
       			<div className={styles.tag}>#Infant</div>
@@ -31,36 +31,45 @@ const Community:FunctionComponent = () => {
       			<div className={styles.tag2}>#Preschool</div>
       			<div className={styles.username}>MamaBear</div>
       			<div className={styles.communityUsername}>TinyGiggles</div>
-      			<div className={styles.username2}>TinyGiggles</div>
+      			<div className={styles.username2}>Happymama</div>
       			<div className={styles.timestamp}>1 minute ago</div>
       			<div className={styles.communityTimestamp}>10 minute ago</div>
       			<div className={styles.timestamp2}>10 minute ago</div>
       			<div className={styles.comments}>20 comments</div>
-      			<div className={styles.communityComments}>10 comments</div>
+      			<div className={styles.communityComments}>24 comments</div>
       			<div className={styles.comments2}>10 comments</div>
       			<div className={styles.views}>101 views</div>
       			<div className={styles.communityViews}>101 views</div>
       			<div className={styles.views2}>101 views</div>
-      			<img className={styles.communityChild} alt="" />
-      			<img className={styles.communityItem} alt="" />
-      			<img className={styles.communityInner} alt="" />
-      			<img className={styles.imageIcon} alt="" />
-      			<img className={styles.communityImageIcon} alt="" />
-      			<img className={styles.imageIcon2} alt="" />
+      			<img className={styles.communityChild} src="/glowmom/icons/communityChild.png" alt="" />
+      			<img className={styles.communityItem} 
+				src="/glowmom/icons/expert_b.png" 
+				alt="" />
+      			<img className={styles.communityInner} 
+				src="/glowmom/icons/profile2.png"alt="" />
+      			<img className={styles.imageIcon} 
+				src="/glowmom/icons/babeOne.png" alt="" />
+      			<img className={styles.communityImageIcon} src="/glowmom/icons/post2.png" alt="" />
+      			<img className={styles.imageIcon2}
+				src="/glowmom/icons/post3.png"
+				alt="hello" />
       			<div className={styles.divider} />
       			<div className={styles.communityDivider} />
-      			<img className={styles.icon} alt="" />
-      			<img className={styles.communityIcon} alt="" />
-      			<img className={styles.icon2} alt="" />
-      			<img className={styles.icon3} alt="" />
-      			<img className={styles.icon4} alt="" />
-      			<img className={styles.icon5} alt="" />
-      			<img className={styles.vectorIcon} alt="" />
-      			<img className={styles.communityCommunityChild} alt="" />
+      			<img className={styles.icon} src="/glowmom/icons/heart.png"alt="" />
+      			<img className={styles.communityIcon} 
+				 alt="" />
+      			<img className={styles.icon2} src="/glowmom/icons/heart.png" alt="" />
+      			<img className={styles.icon3} src="/glowmom/icons/icon4.png" alt="" />
+      			<img className={styles.icon4} src="/glowmom/icons/icon4.png" alt="" />
+      			<img className={styles.icon5} src="/glowmom/icons/icon4.png" alt="" />
+      			<img className={styles.vectorIcon} src="/glowmom/icons/heart.png"alt="" />
+      			
       			<div className={styles.container}>
         				<div className={styles.background} />
         				<b className={styles.title}>Community</b>
-        				<img className={styles.icon6} alt="" />
+        				<img className={styles.icon6} 
+						src="/glowmom/icons/icon6.png"
+						alt="" />
       			</div>
       			<div className={styles.communityContainer}>
         				<div className={styles.container2}>
@@ -70,11 +79,12 @@ const Community:FunctionComponent = () => {
           					<div className={styles.label2}>Infant</div>
           					<div className={styles.label3}>Toddler</div>
           					<div className={styles.label4}>Preschool</div>
-          					<img className={styles.icon7} alt="" onClick={onIconClick} />
-          					<img className={styles.icon8} alt="" />
-          					<img className={styles.icon9} alt="" />
-          					<img className={styles.icon10} src="./icons/icon10.png" alt="fuck" />
-          					<img className={styles.icon11} src="./icons/icon11.png" alt="" />
+          					<img className={styles.icon7} src="/glowmom/icons/icon10.png" alt="" onClick={onIconClick} />
+          					<img className={styles.icon8} src="/glowmom/icons/icon10.png" alt="" />
+          					<img className={styles.icon9} 
+							src="/glowmom/icons/icon9.png" alt="" />
+          					<img className={styles.icon10} src="/glowmom/icons/icon10.png" alt="prenatal" />
+          					<img className={styles.icon11} src="/glowmom/icons/icon11.png" alt="" />
         				</div>
       			</div>
       			<div className={styles.background2} />
@@ -82,18 +92,23 @@ const Community:FunctionComponent = () => {
       			<div className={styles.subtitle}>Quick insights from child-care experts</div>
       			<div className={styles.container3}>
         				<div className={styles.viewMore}>View more</div>
-        				<img className={styles.vectorStrokeIcon} alt="" />
+        				<img className={styles.vectorStrokeIcon}
+						src="/glowmom/icons/stroke.png" alt="" />
       			</div>
       			<div className={styles.container4}>
         				<div className={styles.card} />
         				<div className={styles.communityCard} />
         				<div className={styles.card2} />
         				<div className={styles.card3} />
-        				<img className={styles.containerChild} alt="" />
-        				<img className={styles.containerItem} alt="" />
-        				<div className={styles.author}>Dr. Jules Peterson</div>
-        				<img className={styles.containerInner} alt="" />
-        				<img className={styles.ellipseIcon} alt="" />
+        				<img className={styles.containerChild} src="/glowmom/icons/profile2.png"  alt="" />
+        				<img className={styles.containerItem} src="/glowmom/icons/expert_a.png" alt="" />
+        				<div className={styles.author}>Dr. Leaane O'Neil</div>
+        				<img className={styles.containerInner} 
+						src="/glowmom/icons/user4.png"
+						alt="" />
+        				<img className={styles.ellipseIcon} 
+						src="/glowmom/icons/communityChild.png"
+						alt="" />
         				<div className={styles.title2}>Is Frequent Night Waking Normal?</div>
           					<div className={styles.communityAuthor}>Dr. Susan Collins</div>
           					<div className={styles.author2}>Dr. Jules Peterson</div>
@@ -103,26 +118,26 @@ const Community:FunctionComponent = () => {
               							<div className={styles.title5}>Is Frequent Night Waking Normal?</div>
                 								</div>
                 								<img
-  className={styles.containerIcon}
-  src="/icons/containerIcon.svg"
-  alt="createPost"
-  onClick={() => navigate("/post")}   // ✅ this matches your route
-/>
+												className={styles.containerIcon}
+												src="/glowmom/icons/containerIcon.png"
+												alt="createPost"
+ 												 onClick={() => navigate("/post")}   
+																							/>
 
 
                 								<div className={styles.statusBar}>
-                  									<img className={styles.frameIcon} src="./icons/frameIcon.svg" alt="" />
+                  									<img className={styles.frameIcon} src="/glowmom/icons/status.png" alt="" />
                   									<div className={styles.action}>
                     										<div className={styles.time}>9:41</div>
                   									</div>
                 								</div>
                 								<div className={styles.container5}>
-                  									<img className={styles.icon12} alt="" onClick={onIconClick} />
-                  									<img className={styles.icon13} alt="" />
+                  									<img className={styles.icon12} alt="" src="/glowmom/icons/icon12.png"  onClick={onIconClick} />
+                  									<img className={styles.icon13} src="/glowmom/icons/icon13.png"  alt="" />
                   									<div className={styles.home} onClick={onIconClick}>
-                    										<img className={styles.icon14} src="./icons/icon14.svg" alt="" />
+                    										<img className={styles.icon14} src="/glowmom/icons/icon14.png" alt="" />
                   									</div>
-                  									<img className={styles.icon15} alt="" />
+                  									<img className={styles.icon15} src="/glowmom/icons/icon15.png" alt="" />
                   									<div className={styles.divider2} />
                 								</div>
                 								</div>);
--- src/Posts.tsx
+++ src/Posts.tsx
@@ -16,101 +16,32 @@ const Posts:FunctionComponent = () => {
         				<b className={styles.createPost}>Create Post</b>
         				<div className={styles.post2} onClick={onPostTextClick}>Post</div>
       			</div>
-      			<img className={styles.lixIcon} src="/icons_post/li_x.svg"alt="" onClick={() => navigate("/")} />
+      			<img className={styles.lixIcon} src="/glowmom/icons/backspace.png"alt="" onClick={() => navigate("/")} />
       			<div className={styles.mypancakeParent}>
         				<div className={styles.mypancake}>MyPancake</div>
-        				<img className={styles.groupChild} alt="" />
+        				<img className={styles.groupChild} 
+						src="/glowmom/icons/user4.png" alt="" />
       			</div>
       			<div className={styles.whatsOnYour} onClick={onPostTextClick}>What’s on your mind?</div>
         				<div className={styles.image} />
         				<div className={styles.postChild} />
-        				<img className={styles.postItem} alt="" />
+        				<img className={styles.postItem} src="/glowmom/icons/category.png"alt="" />
         				<div className={styles.category}>Category</div>
-        				<img className={styles.licameraIcon} alt="" />
+        				<img className={styles.licameraIcon}
+						src="/glowmom/icons/camera.png"  alt="" />
         				<div className={styles.iphoneXsKeyboardsDefaul}>
-          					<img className={styles.backgroundsKeyboardsGray} alt="" />
-          					<div className={styles.keys}>
-            						<div className={styles.symbols}>
-              							<div className={styles.postBackground} />
-              							<div className={styles.div}>123</div>
-            						</div>
-            						<div className={styles.space}>
-              							<div className={styles.background2} />
-              							<div className={styles.postSpace}>space</div>
-            						</div>
-            						<div className={styles.button}>
-              							<div className={styles.background3} />
-              							<div className={styles.label}>Label</div>
-            						</div>
-          					</div>
-          					<img className={styles.emojiGlyphIcon} alt="" />
-          					<img className={styles.dictationGlyphIcon} alt="" />
-          					<div className={styles.firstRow}>
-            						<img className={styles.backgroundIcon} alt="" />
-            						<img className={styles.backgroundIcon} alt="" />
-            						<img className={styles.backgroundIcon} alt="" />
-            						<img className={styles.backgroundIcon} alt="" />
-            						<img className={styles.backgroundIcon} alt="" />
-            						<img className={styles.backgroundIcon} alt="" />
-            						<img className={styles.backgroundIcon} alt="" />
-            						<img className={styles.backgroundIcon} alt="" />
-            						<img className={styles.backgroundIcon} alt="" />
-            						<img className={styles.backgroundIcon} alt="" />
-            						<div className={styles.p}>P</div>
-            						<div className={styles.o}>O</div>
-            						<div className={styles.i}>I</div>
-            						<div className={styles.u}>U</div>
-            						<div className={styles.y}>Y</div>
-            						<div className={styles.t}>T</div>
-            						<div className={styles.r}>R</div>
-            						<div className={styles.e}>E</div>
-            						<div className={styles.w}>W</div>
-            						<div className={styles.q}>Q</div>
-          					</div>
-          					<div className={styles.secondRow}>
-            						<img className={styles.backgroundIcon} alt="" />
-            						<img className={styles.backgroundIcon} alt="" />
-            						<img className={styles.backgroundIcon} alt="" />
-            						<img className={styles.backgroundIcon} alt="" />
-            						<img className={styles.backgroundIcon} alt="" />
-            						<img className={styles.backgroundIcon} alt="" />
-            						<img className={styles.backgroundIcon} alt="" />
-            						<img className={styles.backgroundIcon} alt="" />
-            						<img className={styles.backgroundIcon} alt="" />
-            						<div className={styles.l}>L</div>
-            						<div className={styles.k}>K</div>
-            						<div className={styles.j}>J</div>
-            						<div className={styles.h}>H</div>
-            						<div className={styles.g}>G</div>
-            						<div className={styles.f}>F</div>
-            						<div className={styles.d}>D</div>
-            						<div className={styles.s}>S</div>
-            						<div className={styles.a}>A</div>
-          					</div>
-          					<div className={styles.thirdRow}>
-            						<img className={styles.backgroundIcon} alt="" />
-            						<img className={styles.backgroundIcon} alt="" />
-            						<img className={styles.backgroundIcon} alt="" />
-            						<img className={styles.backgroundIcon} alt="" />
-            						<img className={styles.backgroundIcon} alt="" />
-            						<img className={styles.backgroundIcon} alt="" />
-            						<img className={styles.backgroundIcon} alt="" />
-            						<img className={styles.keyboardsShift} alt="" />
-            						<div className={styles.m}>M</div>
-            						<div className={styles.n}>N</div>
-            						<div className={styles.b}>B</div>
-            						<div className={styles.v}>V</div>
-            						<div className={styles.c}>C</div>
-            						<div className={styles.x}>X</div>
-            						<div className={styles.z}>Z</div>
-            						<img className={styles.keyboardsDelete} alt="" />
-          					</div>
+          					<img className={styles.backgroundsKeyboardsGray} 
+							src="/glowmom/icons/keyboard.png"alt="" />
+          					
         				</div>
-        				<div className={styles.postInner} />
+        				<div className={styles.postInner}/>
         				<div className={styles.rectangleDiv} onClick={onPostTextClick} />
-        				<img className={styles.liimageMinusIcon} alt="" />
+        				<img className={styles.liimageMinusIcon} 
+						src="/glowmom/icons/album.png" alt="" />
         				<div className={styles.statusBar}>
-          					<img className={styles.frameIcon} alt="" />
+          					<img className={styles.frameIcon} 
+							src="/glowmom/icons/status.png"
+							alt="" />
           					<div className={styles.action}>
             						<div className={styles.time}>9:41</div>
           					</div>
